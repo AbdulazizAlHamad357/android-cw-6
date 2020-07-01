25ffe18a1d5407d2d021046c781f6731c0d93287
AbdulazizAlHamad357 <abdulazizr.alhamad@student.bbs.edu.kw>
2020-07-01 17:52:33 +0300

    CW6

---
+++ movie/Movie.java
@@ -0,0 +1,10 @@
+package com.example.movie;
+
+public class Movie {
+
+    private String Title;
+    private String mainActor;
+    private Double movieRate;
+    private Int pgRate;
+    private String Genre;
+}
